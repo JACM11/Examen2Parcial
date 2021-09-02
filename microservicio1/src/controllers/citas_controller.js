d3235aeba2189b78bcf9728411836bffcb27f356
JACM11 <joseabel200059@gmail.com>
2021-09-02 13:11:58 -0500

    Tercer commit

--- microservicio1/src/controllers/citas_controller.js
+++ microservicio1/src/controllers/citas_controller.js
@@ -4,35 +4,38 @@ class Cita{
     //listar citas
     async listar(req,res){
         const citas = await Citas.find();
-        res.render('citas',{data:citas});
+        res.render('view_citas',{data:citas});
     }
 
     //agregar citas
     async add(req,res){
         await Citas.create(req.body);
-        res.redirect('/citas')
+        res.redirect('/api/v1/view_citas')
     }
 
     //actualizar citas
     async actualizar(req,res){
         await Citas.update(req,res);
-        res.redirect('/citas')
+        res.redirect('/api/v1/view_citas')
     }
 
     //eliminar citas
     async eliminar(req,res){
         await Citas.delete(req,res);
-        res.redirect('/citas')
+        res.redirect('/api/v1/view_citas')
     }
 
     //buscar entre fechas
     async buscarentrefecha(req,res){
-        await Citas.find({
+        const citas =await Citas.find({
             created_at: {
                 $gte: req.body.fecha_inicio,
                 $lt: req.body.fecha_fin
+
             }
+            
         });
+        res.render('view_citas',{data:citas});
     }
 }
 module.exports = Cita;
